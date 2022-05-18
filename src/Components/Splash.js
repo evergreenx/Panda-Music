49193a0ec4dbbc5980674ffa2a6dc2405d04b26d
Ido Evergreen <dicksonevergreenido@gmail.com>
2022-05-18 06:15:50 +0100

    done with splash screens

--- src/Components/Splash.js
+++ src/Components/Splash.js
@@ -7,7 +7,7 @@ import { LogBox } from "react-native";
 
 LogBox.ignoreLogs(["exported from 'deprecated-react-native-prop-types'."]);
 
-export default function Splash() {
+export default function Splash({ navigation }) {
   const animation = useRef(null);
 
   useEffect(() => {
@@ -19,14 +19,28 @@ export default function Splash() {
       <FocusedStatusBar background={COLORS.primaryLight} />
 
       <View
-        style={{ flex: 1, justifyContent: "center", alignContent: "center" }}
+        style={{ flex: 1, justifyContent:
+            
+                  
+                  "center", alignContent: "center",
+            width: "100%", height: "100%"
+              }}
       >
         <LottieView
           source={require("../Assets/image/lf20_3zqh4wzr.json")}
           autoPlay
           ref={animation}
-          //   loop={false}
-          speed={10}
+          loop={false}
+          speed={2}
+                  duration={4000}
+                
+              
+          resizeMode="center"
+          onAnimationFinish={() => {
+            console.log("animation finished");
+              navigation.navigate("Home");
+              
+          }}
         />
       </View>
     </SafeAreaView>
